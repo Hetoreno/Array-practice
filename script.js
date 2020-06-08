94d4f2c923ba186f3ead1ead5b947af66868c4cc
Hetoreno <hectortamriel@gmail.com>
2020-06-08 16:21:30 -0700

    adding folders

---
+++ script.js
@@ -0,0 +1,21 @@
+var myArray = new Array();
+myArray[0]= 'pizza';
+myArray[1]= 'chocolate';
+/*
+alert(myArray[0]);
+*/
+console.log(myArray);
+console.log(myArray.length);
+
+///////
+var tweets= ["Hello everyone", "I am not sick!"];
+
+tweets.push("Im so tired!");
+tweets.splice(1, 1, "Yeeeeh");
+
+console.log(tweets[2]);
+console.log(tweets);
+
+/*
+alert(tweets[1]);
+*/
